f4a3952967ef5d752f9189b27be8c9a12390ea73
lirdi <38312870+lirdib@users.noreply.github.com>
2020-10-29 16:07:43 +0100

    u krijua faqja sign in

--- bookmarketplace/bookmarketplace/views.py
+++ bookmarketplace/bookmarketplace/views.py
@@ -1,7 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
 
 def homepage(request):
     return render(request,'main.html')
+<<<<<<< HEAD
+
+def register(request):
+    form = UserCreationForm()
+    return render(request,'signup.html',{'form':form})
+=======
 def register(request):
     return render(request,'signup.html')
+>>>>>>> be25670b5c1e790a3c22ec9e1a1be8e6ebadf712
