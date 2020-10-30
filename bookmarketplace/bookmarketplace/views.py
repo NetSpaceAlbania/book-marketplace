d62485df923df6a60925272bebc25b1d48f59299
lirdi <38312870+lirdib@users.noreply.github.com>
2020-10-30 08:31:37 +0100

    Update views.py

--- bookmarketplace/bookmarketplace/views.py
+++ bookmarketplace/bookmarketplace/views.py
@@ -5,12 +5,12 @@ from django.contrib.auth.forms import UserCreationForm
 
 def homepage(request):
     return render(request,'main.html')
-<<<<<<< HEAD
+
 
 def register(request):
     form = UserCreationForm()
     return render(request,'signup.html',{'form':form})
-=======
+
 def register(request):
     return render(request,'signup.html')
->>>>>>> be25670b5c1e790a3c22ec9e1a1be8e6ebadf712
+
