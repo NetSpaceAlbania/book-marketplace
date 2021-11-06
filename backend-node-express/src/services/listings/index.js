fa004696627ab322c712264b3647f86ed27d3a80
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-06 02:15:40 +0100

    PUT method (small bug to fix)

--- backend-node-express/src/services/listings/index.js
+++ backend-node-express/src/services/listings/index.js
@@ -23,7 +23,7 @@ listingsRouter.post("/", validateListing, async (req, res, next) => {
     const errorList = validationResult(req);
     console.log("Error list: ", errorList);
     if (errorList.isEmpty()) {
-      // read the the content of listings.jsonrs
+      // read the the content of listings.jsons
       const listings = await readListings();
       console.log("Listings consol log: ", listings);
       // read the requests body
@@ -35,7 +35,7 @@ listingsRouter.post("/", validateListing, async (req, res, next) => {
       // push the new listing to the listings array
       listings.push(newListing);
       // write the new listings array to the file listings.json
-       await writeListings(listings);
+      await writeListings(listings);
       // send the new listing to the client
       res.status(201).send(newListing);
     } else {
@@ -43,18 +43,72 @@ listingsRouter.post("/", validateListing, async (req, res, next) => {
     }
   } catch (err) {
     console.log(err);
-    next(err);
+    next(err); // pass the error to the next middleware (error handlers imported in server.js from errorHandling.js)
   }
 });
 
 // ********* GET ALL LISTINGS ********************
 listingsRouter.get("/", async (req, res, next) => {
   try {
+    // read the the content of listings.jsons
     const listings = await readListings();
+    // send the listings to the client
     res.status(200).send(listings);
   } catch (error) {
     console.log(error);
-    next(error);
+    next(error); // pass the error to the next middleware (error handlers imported in server.js from errorHandling.js)
+  }
+});
+
+// GET A SPECIFIC LISTING
+listingsRouter.get("/:id", async (req, res, next) => {
+  try {
+    // read the the content of listings.jsons
+    const listings = await readListings();
+    // find the listing with the id in the request params
+    const listing = listings.find((listing) => listing.id === req.params.id);
+    // if the listing is found send it to the client
+    if (listing) {
+      res.status(200).send(listing);
+    } else {
+      next(createHttpError(404, "Listing not found"));
+    }
+  } catch (error) {
+    console.log(error);
+    next(error); // pass the error to the next middleware (error handlers imported in server.js from errorHandling.js)
+  }
+});
+
+// ********* UPDATE A SPECIFIC LISTING ********************
+listingsRouter.put("/:id", validateListing, async (req, res, next) => {
+  try {
+    const errorList = validationResult(req);
+    if (errorList.isEmpty()) {
+      // read the the content of listings.jsons
+      const listings = await readListings();
+      // find the listing with the id in the request params
+      const listing = listings.find((listing) => listing.id === req.params.id);
+      // update the listing with the new information
+      // the updatedListing is the merge of the copied object of the current listing that matches the id, with the copied object of the body request that will overwrite some part or everything of the original
+      const updatedListing = {
+        ...listing,
+        ...req.body,
+        updatedAt: new Date(),
+      };
+      // the remaining listings - all the listings apart the one we want to modify, the one that matches the id
+      const remainingListings = listings.filter(
+        (listing) => listing.id !== req.params.id
+      );
+      // push the updated listing to the listings array
+      remainingListings.push(updatedListing);
+    }
+    // save the new updated listings array to the file listings.json
+    await writeListings(remainingListings);
+    // send the updated listing to the client
+    res.status(200).send(updatedListing);
+  } catch (error) {
+    console.log(error);
+    next(error); // pass the error to the next middleware (error handlers imported in server.js from errorHandling.js)
   }
 });
 
