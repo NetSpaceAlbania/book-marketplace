da7dc1dfa56ace3f43e64ae1ce706de5ec044bcd
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-07 16:48:50 +0100

    GET ALL users

--- backend-node-express/src/db/routes/users.js
+++ backend-node-express/src/db/routes/users.js
@@ -1,58 +1,78 @@
 import express from "express";
-import User from "../models/User.js"
-import verify from "../../verifyToken.js"
+import User from "../models/User.js";
+import verify from "../../verifyToken.js";
 
 const userRouter = express.Router();
 
 // *************** UPDATE ********************
 userRouter.put("/:id", verify, async (req, res) => {
-    if (req.user.id === req.params.id || req.user.isAdmin) {
-      if (req.body.password) {
-        req.body.password = CryptoJS.AES.encrypt(
-          req.body.password,
-          process.env.SECRET_KEY
-        ).toString();
-      }
-      try {
-        const updatedUser = await User.findByIdAndUpdate(
-          req.params.id,
-          {
-            $set: req.body,
-          },
-          { new: true }
-        );
-        res.status(200).json(updatedUser);
-      } catch (err) {
-        res.status(500).json(err);
-      }
-    } else {
-      res.status(403).json("You can update only your account!");
+  if (req.user.id === req.params.id || req.user.isAdmin) {
+    if (req.body.password) {
+      req.body.password = CryptoJS.AES.encrypt(
+        req.body.password,
+        process.env.SECRET_KEY
+      ).toString();
     }
-  });
-
-  // *************** DELETE ********************
-userRouter.delete("/:id", verify, async (req, res) => {
-    if (req.user.id === req.params.id || req.user.isAdmin) {
-      try {
-        await User.findByIdAndDelete(req.params.id);
-        res.status(200).json("User has been deleted");
-      } catch (err) {
-        res.status(500).json(err);
-      }
-    } else {
-      res.status(403).json("You can delete only your account!");
-    }
-  });
-
-  // *************** GET SPECIFIC USER ********************
-userRouter.get("/find/:id", async (req, res) => {
     try {
-      const user = await User.findById(req.params.id);
-      const { password, ...info } = user._doc;
-      res.status(200).json(info);
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedUser);
     } catch (err) {
       res.status(500).json(err);
     }
-  });
+  } else {
+    res.status(403).json("You can update only your account!");
+  }
+});
+
+// *************** DELETE ********************
+userRouter.delete("/:id", verify, async (req, res) => {
+  if (req.user.id === req.params.id || req.user.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("User has been deleted");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You can delete only your account!");
+  }
+});
+
+// *************** GET SPECIFIC USER ********************
+userRouter.get("/find/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...info } = user._doc;
+    res.status(200).json(info);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// *************** GET ALL USERS ************************
+// if no query is sent, is just gonna return ALL users. But if you add for example "/?new=true", is gonna return only last 10 users
+userRouter.get("/", verify, async (req, res) => {
+  const query = req.query.new; // (or new users) new is the key
+  if (req.user.isAdmin) {
+    try {
+      // if there is a query (which means if we are fetching only new users), is gonna fetch only last 10 users
+      // if there is no query, is gonna fetch all users
+      const users = query
+        ? await User.find().sort({ _id: -1 }).limit(10)
+        : await User.find();
+      res.status(200).json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed to see all users!");
+  }
+});
 
 export default userRouter;
