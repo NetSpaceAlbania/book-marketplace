a5b2dc315d6abbb1e17e089a90007aad6f1061cb
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-07 16:46:27 +0100

    DELETE user method

--- backend-node-express/src/db/routes/users.js
+++ backend-node-express/src/db/routes/users.js
@@ -30,4 +30,18 @@ userRouter.put("/:id", verify, async (req, res) => {
     }
   });
 
+  // ***************DELETE********************
+userRouter.delete("/:id", verify, async (req, res) => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(403).json("You can delete only your account!");
+    }
+  });
+
 export default userRouter;
