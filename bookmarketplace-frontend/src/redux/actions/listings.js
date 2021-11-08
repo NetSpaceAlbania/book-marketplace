5f0de5f1747ee290214e1e9b5ffabae99f8d260b
theringsofsaturn <emiliankasemi@gmail.com>
2021-11-08 20:39:16 +0100

    Redux endpoint for listings corrected

--- bookmarketplace-frontend/src/redux/actions/listings.js
+++ bookmarketplace-frontend/src/redux/actions/listings.js
@@ -1,48 +1,46 @@
-import axios from 'axios';
+import axios from "axios";
 import {
-    GET_LISTING_LOADING,
-    GET_LISTING_SUCCESS,
-    GET_LISTING_FAIL
-} from './types';
+  GET_LISTING_LOADING,
+  GET_LISTING_SUCCESS,
+  GET_LISTING_FAIL,
+} from "./types";
 
-import url from '../constants/environment';
-import { useToken } from '../customHooks';
-
+import url from "../constants/environment";
+import { useToken } from "../customHooks";
 
 let config = {
-    headers: {
-        'Content-Type': 'application/json',
-    }
-}
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
 
-export const getListings = () => async dispatch => {
+export const getListings = () => async (dispatch) => {
+  let token = useToken();
 
-    let token = useToken();
+  config.headers = {
+    ...config.headers,
+    Authorization: `JWT ${token}`,
+  };
+  console.log(config);
 
-    config.headers = {
-        ...config.headers, 
-        'Authorization': `JWT ${token}`
-    }
-    console.log(config);
+  const body = JSON.stringify({});
 
-    const body = JSON.stringify({}); 
+  dispatch({
+    type: GET_LISTING_LOADING,
+  });
+
+  try {
+    const res = await axios.get(`${url}/listings`, config);
+
+    console.log("The DATA fetched succesfully!");
 
     dispatch({
-        type: GET_LISTING_LOADING,
+      type: GET_LISTING_SUCCESS,
+      payload: res.data,
     });
-
-    try {
-        const res = await axios.get(`${url}/api/book-list/`, config);
-
-        console.log("success");
-
-        dispatch({
-            type: GET_LISTING_SUCCESS,
-            payload: res.data
-        });
-    } catch (err) {
-        dispatch({
-            type: GET_LISTING_FAIL
-        });
-    }
+  } catch (err) {
+    dispatch({
+      type: GET_LISTING_FAIL,
+    });
+  }
 };
--- bookmarketplace-frontend/src/redux/constants/environment.js
+++ bookmarketplace-frontend/src/redux/constants/environment.js
@@ -1,4 +1,4 @@
-export const url = "http://localhost:8000"
+export const url = "http://localhost:3001"
 // const url = "http://159.89.31.70:80";
 
 export default url;
